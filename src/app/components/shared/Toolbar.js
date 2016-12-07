f3d86f9a5a521ea7a16b5cb0d3872b802db7c531
Stone Chen <yeegr@hotmail.com>
2016-12-07 20:31:39 +0800

    toolbar

--- src/app/components/shared/Toolbar.js
+++ src/app/components/shared/Toolbar.js
@@ -227,7 +227,6 @@ const styles = StyleSheet.create({
   wrapper: {
     alignItems: 'center',
     backgroundColor: 'transparent',
-    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around'
   }
