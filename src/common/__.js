088c343f853fa76f4494610de3624cac59abdccd
Stone Chen <yeegr@hotmail.com>
2017-02-23 17:52:10 +0800

    localization

--- src/common/__.js
+++ src/common/__.js
@@ -1,30 +1,28 @@
 'use strict'
 
 export const CONSTANTS = require('./constants')
-
 export const UTIL = require('./util')
-
 export const FETCH = require('./fetch').FETCH
+export const Defaults = require('./defaults.json')
+export const Lang = require('../locales/zh-CN.json')
+const LANG = require('../locales/zh-CN.1.json')
 
-export let Lang = require('../locales/zh-CN.json')
+let env = require('./env.json'),
+  servers = env['development']
 
 export let AppSettings = require('./settings.json')
 AppSettings.mobileRx = new RegExp(/^1+\d{10}$/)
 AppSettings.vcodeRx = new RegExp('\\d{' + AppSettings.verificationCodeLength + '}', 'g')
 AppSettings.pidRx = new RegExp(/\d{18}/)
 AppSettings.trailTypes = [0,1,2,3,4,5,6,7,8,9]
-AppSettings.paymentMethods = Lang.payments
-
-let env = require('./env.json'),
-  servers = env['staging']
 
 AppSettings.baseUri = servers.WEB_SERVER + '#/'
 AppSettings.apiUri = servers.API_SERVER
 AppSettings.assetUri = servers.ASSET_SERVER
 AppSettings.imageUri = servers.IMAGE_SERVER
 
+AppSettings.paymentMethods = LANG.order.paymentMethods
+
 export let Graphics = require('./graphics.json')
 Graphics.page = {}
 Graphics.page.marginTop = Graphics.statusbar.height + Graphics.titlebar.height
-
-export const Defaults = require('./defaults.json')
